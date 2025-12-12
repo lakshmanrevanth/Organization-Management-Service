bbb4660c84f61a18f022f252a9a4d14d5c7935df
lakshmanrevanth <lakshman6668@gmail.com>
2025-12-13 04:46:35 +0530

    database connection solved

--- core/config.py
+++ core/config.py
@@ -5,10 +5,11 @@ load_dotenv()
 
 class Settings:
     MONGODB_URI = os.getenv("MONGODB_URI")
-    MASTER_DB = os.getenv("MASTER_DB_NAME", "organization_master")
+    MASTER_DB = os.getenv("MASTER_DB", "organization_master")
 
-    SECRET_KEY = os.getenv("SECRET_KEY", "change_me")
+    SECRET_KEY = os.getenv("SECRET_KEY")
     ALGORITHM = "HS256"
-    TOKEN_EXPIRE_MINUTES = 60
+    TOKEN_EXPIRE_MINUTES = int(os.getenv("TOKEN_EXPIRE_MINUTES", 60))
+
 
 settings = Settings()
--- database/mongo.py
+++ database/mongo.py
@@ -1,7 +1,10 @@
 from pymongo import MongoClient
 from core.config import settings
 
-client = MongoClient(settings.MONGODB_URI)
+client = MongoClient(
+    settings.MONGODB_URI,
+    serverSelectionTimeoutMS=5000
+)
 
 def get_master_db():
     return client[settings.MASTER_DB]
